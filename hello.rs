b45519bd23b9ce892e7ca6829e8378271339a42b
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-13 10:01:12 +0100

    rust

--- hello.rs
+++ hello.rs
@@ -45,4 +45,36 @@ fn main() {
     println!("interest is {}", interest);
     println!("integer is assigned to a float type");
     */
+    println!("=====number seperator====");
+    let num1:i32 = 2_00_0_00; // the order of underscore doesnt matter
+    let num2:f32 = 100_000.345_002;
+    println!("num1 is {}", num1);
+    println!("num2 is {}", num2);
+    println!("=====constant=====");
+    const PI:f32 = 3.14;
+    println!("pi is {}", PI);
+    println!("=======shadowing variables======");
+    let _num1 = 1000;
+    let num1 = 500;
+    print!("{}", num1);
+    println!("Without using `println`");
+    let uname = "Prime";
+    let uname = uname.len();
+    println!("name is of length: {}", uname);
+    let uname:&str = "Prime";
+    let uname:usize = uname.len();
+    println!("name is of length: {}", uname);
+    println!("======STATIC======");
+    let company_name = "The Primotion Studio"; // By default, all string
+    let persona:&str = "Primotion Studio";     // literals are of static types
+    let person:&'static str = "Prime";         // but us can explicitly specify that it should be
+                                               // static, by using `&'static str`
+    println!("I own a company called `{}`, and i take on the `{}` persona.", company_name, persona);
+    println!("My name is `{}`", person);
+
+    println!("=======String Object=======");
+    let emptystr = String::new();
+    println!("empty string is `{}` -> len: {}", emptystr, emptystr.len());
+    let contentstr = String::from("Primotion STudio");
+    println!("content string is `{}` -> len: {}", contentstr, contentstr.len());
 }
