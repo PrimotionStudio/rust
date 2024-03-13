bfb4744e212868883db228fa8113ecb900d4ea31
The Primotion Studio <oyedelenewton@gmail.com>
2024-03-13 08:11:15 +0100

    first commit

---
+++ hello.rs
@@ -0,0 +1,29 @@
+fn main() {
+    let fname = "prime";
+    let lname = "boss";
+    println!("Hello {} {}", fname, lname);
+
+    let result = 10; // i32 by default
+    let age:u32 = 20;
+    let sum:i32 = 5 - 15;
+    let mark:isize = 10;
+    let count:usize = 30;
+
+    println!("result is {}", result);
+    println!("age is {}", age);
+    println!("sum is {}", sum);
+    println!("mark is {}", mark);
+    println!("count is {}", count);
+
+    println!("=========Integer Overflow=======");
+    let age:u8 = 255;
+    let width:u8 = 256;
+    let height:u8 = 257;
+    let score:u8 = 258;
+
+    println!("Found it");
+    println!("age is {}", age);
+    println!("width is {}", width);
+    println!("height is {}", height);
+    println!("score is {}", score);
+}
