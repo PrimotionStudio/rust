e35f7fed9db4f6e27eac6a7eeea2f7526cbad8f0
The Primotion Studio <oyedelenewton@gmail.com>
2024-04-14 00:53:44 +0100

    so far with rust

---
+++ ownership/target/debug/deps/ownership-143aabecf7a489f1.d
@@ -0,0 +1,5 @@
+/home/primotion/Documents/RUST/ownership/target/debug/deps/libownership-143aabecf7a489f1.rmeta: src/main.rs
+
+/home/primotion/Documents/RUST/ownership/target/debug/deps/ownership-143aabecf7a489f1.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/deps/ownership-af81d5f6a2902833.d
@@ -0,0 +1,5 @@
+/home/primotion/Documents/RUST/ownership/target/debug/deps/libownership-af81d5f6a2902833.rmeta: src/main.rs
+
+/home/primotion/Documents/RUST/ownership/target/debug/deps/ownership-af81d5f6a2902833.d: src/main.rs
+
+src/main.rs:
